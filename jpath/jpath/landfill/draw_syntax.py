8f697bdec9d554219897af74fbe7dcfd5a306503
Alexander Boyd <alex@opengroove.org>
2011-07-17 18:05:16 -0600

    Lot of changes in the grammr

--- jpath/jpath/landfill/draw_syntax.py
+++ jpath/jpath/landfill/draw_syntax.py
@@ -1,5 +1,6 @@
 
 from jpath.query import syntax
+from pango import FontDescription
 
 tail = ["number", "alphanum", "alpha", "digit", "upper", "lower"]
 
@@ -8,6 +9,26 @@ if __name__ == "__main__":
     name = raw_input()
     if not name[-4:] == ".png":
         raise Exception("That doesn't end with .png")
-    syntax.module.draw_productions_to_png({}, name, tail=tail) #@UndefinedVariable
+    """
+        raildraw_production_font=bold_font,
+        raildraw_text_font=bold_font,
+        raildraw_anycase_font=plain_font,
+        raildraw_description_font=plain_font,
+    """
+    plain_font = FontDescription("sans 9")
+    bold_font = FontDescription("sans bold 9")
+    italic_font = FontDescription("sans italic 9")
+    title_font = FontDescription("sans bold 12")
+    syntax.module.draw_productions_to_png({ #@UndefinedVariable
+            "raildraw_production_font": plain_font,
+            "raildraw_text_font": bold_font,
+            "raildraw_anycase_font": plain_font,
+            "raildraw_description_font": italic_font,
+            "raildraw_title_font": title_font,
+            "raildraw_arrow_width": 8,
+            "raildraw_arrow_height": 7,
+            "raildraw_line_size": 1.5,
+            "raildraw_title_after": 40
+            }, name, tail=tail)
     print "done"
     
--- jpath/jpath/query/syntax.py
+++ jpath/jpath/query/syntax.py
@@ -1,11 +1,12 @@
 # coding=UTF-8
 
-from parcon import (Literal, InfixExpr, SignificantLiteral, Forward, 
-        number as p_number, Alphanum, CharNotIn, concat, alpha_chars, 
+from parcon import (Literal, InfixExpr, SignificantLiteral, Forward,
+        number as p_number, Alphanum, CharNotIn, concat, alpha_chars,
         digit_chars, CharIn, digit, Alpha, Upper, Lower, Optional, ZeroOrMore,
-        Keyword, OneOrMore, flatten)
+        Keyword, OneOrMore, flatten, Exact, AnyChar, Except)
 from jpath.query.productions import *
 from collections import namedtuple
+import operator
 
 string_escape_map = {"n": "\n", "r": "\r", "t": "\t", '"': '"', "'": "'"}
 
@@ -46,30 +47,46 @@ upper = Upper()(name="upper")
 lower = Lower()(name="lower")
 alpha = (upper | lower)(name="alpha")
 alphanum = (alpha | digit)(name="alphanum")
+literal_escape = ("\\" + AnyChar())
+id_with_dot = (+(alphanum | CharIn("-_.") | literal_escape))[concat](name="identifier with dot")
+id_no_dot = (+(alphanum | CharIn("-_") | literal_escape))[concat](name="identifier without dot")
+a_id_with_dot = (alpha + -+(alphanum | CharIn("-_.") | literal_escape))[concat](name="alpha identifier with dot")
+a_id_no_dot = (alpha + -+(alphanum | CharIn("-_") | literal_escape))[concat](name="alpha identifier without dot")
 
 expr = Forward()
 
 number = p_number[NumberLiteral]
-string_escape = ("\\" + alphanum)[lookup_string_escape]
+string_escape = ("\\" + (alphanum | '\\' | '"'))[lookup_string_escape]
 string_char = CharNotIn('"\\')(desc='any char except " or \\') | string_escape
-string = ('"' + string_char[...][concat] + '"')(name="string")[StringLiteral]
-var_name = ("$" + +(alphanum | CharIn("-_")))[concat](name="variable")
+string = Exact('"' + string_char[...][concat] + '"')(name="string")[StringLiteral]
+var_name = ("$" + id_no_dot)(name="variable")
 var_reference = var_name[VarReference]
-function_name = (alpha + (alphanum | CharIn("-_."))[...])[concat](name="function name")
+function_name = a_id_with_dot(name="function name")
 function_call = (function_name + "(" + Optional(expr, (None,)) + ")")[create_function_call](name="function call")
-pattern = (alpha + (alphanum | CharIn("-_"))[...])[concat][create_pattern_or_special](name="pattern")
-pair_pattern = ("@" + alpha + (alphanum | CharIn("-_"))[...])[concat][PairPattern](name="pair pattern")
+pattern = a_id_with_dot[create_pattern_or_special](name="pattern")
+pair_pattern = ("@" + a_id_with_dot)[PairPattern](name="pair pattern")
 context_item = Literal(".")[lambda t: ContextItem()](name="context item")
 children = Literal("*")[lambda t: Children()](name="children")
 pair_children = Literal("@*")[lambda t: PairChildren()](name="pair children")
-paren_expr = ("(" + expr + ")")[ParenExpr](name="parens")
+paren_expr = ("(" + expr + ")")[ParenExpr](name="expr in parentheses")
 list_constructor = ("[" + expr + "]")[ListConstructor](name="list")
 empty_list_constructor = (Literal("[") + "]")[lambda t: EmptyListConstructor()](name="empty list")
 object_constructor = ("{" + expr + "}")[ObjectConstructor](name="object")
 empty_object_constructor = (Literal("{") + "}")[lambda t: EmptyObjectConstructor()](name="empty object")
 empty_sequence_constructor = (Literal("(") + ")")[lambda t: EmptySequenceConstructor()](name="empty sequence")
 
-atom = (paren_expr | empty_list_constructor | list_constructor
+xml_node = Forward()
+xml_start_char = (alpha | CharIn(":_"))
+xml_name = (xml_start_char + (xml_start_char | digit | CharIn("-."))[...])[flatten](name="xml name")
+xml_attribute = (xml_name + "=" + string)(name="xml attribute")
+xml_empty_tag = ("<" + xml_name + xml_attribute[...] + "/" + ">")(name="xml empty tag")
+xml_text = OneOrMore(CharNotIn("&<{")(desc="any char except & < {"))[concat](name="xml text")
+xml_expr = ("{" + expr + "}")(name="xml expr")
+xml_contents = (xml_text | xml_node | xml_expr)[...](name="xml contents")
+xml_tag = ("<" + xml_name + xml_attribute[...] + ">" + xml_contents + "<" + "/" + xml_name + ">")(name="xml tag")
+xml_node << (xml_tag | xml_empty_tag)
+
+atom = (paren_expr | xml_node | empty_list_constructor | list_constructor
        | empty_object_constructor | object_constructor
        | empty_sequence_constructor | number | string | var_reference
        | function_call | pattern | pair_pattern | context_item | children
@@ -78,21 +95,21 @@ atom = (paren_expr | empty_list_constructor | list_constructor
 indexer = ("#" + atom)[Indexer](name="indexer")
 pair_indexer = ("@#" + atom)[PairIndexer](name="pair indexer")
 
-infix = (pair_indexer | indexer | atom)(name="term")
+term = (pair_indexer | indexer | atom)(name="term")
 
-infix = (atom + Optional(OneOrMore(
-            ("/" + atom)[PathWrapper] |
+infix = (term + Optional(OneOrMore(
+            ("/" + term)[PathWrapper] | 
             ("[" + expr + "]")[PredicateWrapper]
         ), []))[create_path](name="path")
 
 infix = InfixExpr(infix, [
         ("×" | keyword("times") | keyword("mul"), Multiply),
-        ("÷"| keyword("div") | (keyword("divided") + keyword("by")), Divide)
+        ("÷" | keyword("div") | (keyword("divided") + keyword("by")), Divide)
         ])
 
 infix = InfixExpr(infix, [
         ("+" | keyword("plus") | keyword("add"), Add),
-        ("-"| keyword("minus") | keyword("sub"), Subtract)
+        ("-" | keyword("minus") | keyword("sub"), Subtract)
         ])
 
 infix = InfixExpr(infix, [
@@ -141,9 +158,32 @@ flwor_return = (~keyword("return") + expr)
 flwor_construct = (flwor_for | flwor_let | flwor_where | flwor_order_by | flwor_at | flwor_do)(name="flwor construct")
 flwor = (+(flwor_construct) + flwor_return)(name="flwor")
 
-expr << (if_then_else | satisfies | flwor | infix)(name="expr")
+insert_position = (keyword("before") + expr | keyword("after") + expr | 
+        (Optional(~keyword("at") + (keyword("start") | keyword("end") | 
+                keyword("position") + expr), []) + ~keyword("into") + expr))[list]
+insert = (~keyword("insert") + expr + insert_position)(name="insert")
+delete = (~keyword("delete") + 
+        (~keyword("value") | ~keyword("values")) + expr)(name="delete")
+replace = (~keyword("replace") + ~keyword("value") + expr + ~keyword("with") + expr)(name="replace")
 
-module = expr(name="module")
+update = (insert | delete | replace)(name="update")
+
+expr << (if_then_else | satisfies | update | flwor | infix)(name="expr")
+
+function_arg_def = (Optional(keyword("closure"), "normal") + var_name + 
+        Optional(":=" + atom, (None,)))
+function_arg_def_list = InfixExpr(function_arg_def[lambda x: [x]], [(",", operator.add)])(name="function argument list")
+function_definition = (~keyword("define") + ~keyword("function") + function_name
+        + "(" + Optional(function_arg_def_list, []) + ")" + expr + ~keyword("end"))(name="function definition")
+
+import_type = Except(id_with_dot, keyword("as") | keyword("import"))(name="import type")
+import_source_literal = Except(+((AnyChar() - CharIn(" \r\n\t"))(desc="any non-whitespace char")), keyword("as") | keyword("import"))
+import_source_string = string
+import_source = (import_source_string | import_source_literal)(name="import source")
+import_target = (a_id_no_dot)(name="import target")
+import_statement = (~keyword("import") + (import_type + import_source | import_source) + Optional(~keyword("as") + import_target, ""))(name="import")
+
+module = (import_statement[...] + function_definition[...] + Optional(expr))(name="module")
 
 
 
